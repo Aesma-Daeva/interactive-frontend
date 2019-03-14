e5f2880a10fecba8c594ec4db6d5159bb284cc90
Aesma <aesma.devanshi@gmail.com>
2019-03-14 16:05:53 +0000

    Add button and function to push input in weather \n \n API

--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,16 @@
-$.getJSON("https://api.openweathermap.org/data/2.5/weather?q=Dublin&units=metric&appid=2fa012fec825afa1f975d94bb9232b3f", function(data) {
-    console.log(data);
+function pushData() {
+    var input = document.getElementById("#pac-input");
+    var place = $("#pac-input").val();
 
-    var weatherIcon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-    var weather = data.weather[0].main;
-    var temperature = Math.round(data.main.temp) + "&#8451;";
+    $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + place + "&units=metric&appid=2fa012fec825afa1f975d94bb9232b3f", function(data) {
+        console.log(data);
 
-    $('.weather-icon').attr("src", weatherIcon);
-    $('.weather').append(weather);
-    $('.temperature').append(temperature);
-});
+        var weatherIcon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+        var weather = data.weather[0].main;
+        var temperature = Math.round(data.main.temp) + "&#8451;";
+
+        $('.weather-icon').attr("src", weatherIcon);
+        $('.weather').append(weather);
+        $('.temperature').append(temperature);
+    });
+};
