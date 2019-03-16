f04c1ab46991dd12e2a3740ad8623631dbb80240
Aesma <aesma.devanshi@gmail.com>
2019-03-16 13:13:09 +0000

    Add comments and remove unnecessary code

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,14 +1,23 @@
-function pushData() {
-    var input = document.getElementById("#pac-input");
+//When Show Weather button is clicked, it runs Weather API
+//This API is limited to 60 calls per minute on free subscription so I don't 
+//want it to constantly run whenever the user searches for a place
+function getWeather() {
+    
+    //Gets value from input form search box
     var place = $("#pac-input").val();
 
     $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + place + "&units=metric&appid=2fa012fec825afa1f975d94bb9232b3f", function(data) {
+        //I want to keep this so data always shows in devtools console
         console.log(data);
-
+        
+        //Weather Icon
         var weatherIcon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+        //Weather Name
         var weather = data.weather[0].main;
+        //Current Temperature in Celcius
         var temperature = Math.round(data.main.temp) + "&#8451;";
 
+        //Data shown in the app
         $('.weather-icon').attr("src", weatherIcon);
         $('.weather').append(weather);
         $('.temperature').append(temperature);
