bd0f6cdb7d9f5270cc660e34f6fc35ca35ba957b
Aesma <aesma.devanshi@gmail.com>
2019-04-02 22:22:48 +0000

    Add jumbotron text, background image and style

--- assets/js/noIcon.js
+++ assets/js/noIcon.js
@@ -1,4 +1,9 @@
-/*jslint browser:true */
+/*
+This function was created as a replacement for img alt attribute.
+So if there is no icon shown for weather icon, this error message
+will show instead. I don't want to put alt attribute because it is
+always showing on the  html page and I can't hide it.
+*/
 function noIcon () {
     document.getElementsByClassName("iconError").innerHTML = "The icon has gone with the wind.";
 }
